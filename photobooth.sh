d9b479788cfbbef659176bb66ff2c1d41642d3d6
b9 <hackerb9@member.fsf.org>
2017-05-23 05:58:27 -0700

    If v4l2-ctl is present, increase video quality
    Some webcams return JPEG images with low quality, but have an option
    to be better. Unfortunately, OpenCV doesn't give us access to change
    that, so we have to do it from the shell instead of Python.

--- photobooth.sh
+++ photobooth.sh
@@ -6,6 +6,11 @@ if [[ $1 == "set-time" ]]; then
   sudo python set-time.py
 fi
 
+if v4l2-ctl --silent 2>/dev/null; then
+    # If camera uses JPEG compression, improve the quality  
+    v4l2-ctl -c compression_quality=100 2>/dev/null
+fi    
+
 sudo python photobooth.py >>photobooth.log 2>>photobooth.err
 
 
