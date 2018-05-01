dd0b7ec5131030f832ac3afe5b0be1b3388d0751
Balthasar Reuter <github@re-web.eu>
2018-05-01 22:32:00 +0200

    Structure made compatible to setuptools

--- photobooth/__init__.py
+++ photobooth/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
 import sys
 from .main import main
 
