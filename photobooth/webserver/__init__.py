ddc162339a5b0799a22bb3dfb0cb52dc8cd2ac07
Your Name <oelegeirnaert@hotmail.com>
2019-01-31 22:20:05 +0100

    Cleanup __init__.py

--- photobooth/webserver/__init__.py
+++ photobooth/webserver/__init__.py
@@ -43,42 +43,30 @@ class Webserver(object):
 
     @request_map("/")
     def index():
-        logging.debug("My Root is: %s" % Webserver.root)
-        #testdir = "%s/%s" %(Webserver.p, Webserver.picture_dir)
-        testdir = "pictures"
-        logging.debug(testdir)
-        my_images_path = "%s" %(Webserver.picture_dir)
+        logging.debug("Webserver root is: %s" % Webserver.root)
+
         my_html_file = "%s/templates/index.html" %Webserver.root
 
         image_list = []
-        #for filename in glob.glob('%s/*.jpg' % Webserver.picture_dir): #assuming gif
-            #image_list.append(filename)
         image_list=fnmatch.filter(os.listdir(Webserver.picture_dir), '*.jpg')
         str_image_list = ""
         for i in image_list:
             str_image_list += "<img src='%s' width='100px' />" % i
             logging.debug(i)
         logging.debug(str_image_list)
-        my_image_list = ''.join(image_list)
 
         my_html_stream = open(my_html_file, 'r').read()
         my_html_stream = my_html_stream.replace("[load_images_from_directory]", str_image_list)
-        logging.debug(my_html_file)
+
         return my_html_stream
-        #return {"code": 0, "message": "success - images will be loaded from %s" %os.listdir(testdir)} # You can return a dictionary, a string or a `simple_http_server.simple_http_server.Response` object.
 
     @request_map("{picture}")
     def return_picture(picture=PathValue()):
-        my_images_path = "%s" %(Webserver.picture_dir)
-        my_picture = "%s/%s" % (my_images_path, picture)
+        my_pictures_path = "%s" %(Webserver.picture_dir)
+        my_picture = "%s/%s" % (my_pictures_path, picture)
         logging.debug("Loading picture from: %s" %my_picture)
         return StaticFile(my_picture, "image/jpg")
 
-
-
     def run(self):
-
-        logging.debug("Change HTTP basepath %s" %Webserver.p)
         server.start()
-
         return True
