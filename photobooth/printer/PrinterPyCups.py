a038f3d10e7f0e055d93866add70d2ba3a3a5901
Balthasar Reuter <github@re-web.eu>
2019-05-11 22:26:49 +0200

    PrinterPyCups a bit simplified. Follow-up to #113

--- photobooth/printer/PrinterPyCups.py
+++ photobooth/printer/PrinterPyCups.py
@@ -40,30 +40,28 @@ class PrinterPyCups(Printer):
     def __init__(self, page_size, print_pdf=False):
 
         self._conn = cups.Connection() if cups else None
-        self._counter = 0
-
-        if os.access('/dev/shm', os.W_OK):
-            logging.debug('Storing temp files to "/dev/shm/print.jpg"')
-            self._tmp_filename = '/dev/shm/print.jpg'
-        else:
-            logging.debug('Storing temp files to "/tmp/print.jpg"')
-            self._tmp_filename = '/tmp/print.jpg'
-
-        if self._conn is not None:
-            self._printer = self._conn.getDefault()
-
-            logging.info('Using printer "%s"', self._printer)
 
         if print_pdf:
             logging.error('Printing to PDF not supported with pycups')
+            self._conn = None
+
+        if os.access('/dev/shm', os.W_OK):
+            self._tmp_filename = '/dev/shm/print.jpg'
+        else:
+            self._tmp_filename = '/tmp/print.jpg'
+        logging.debug('Storing temp files to "{}"'.format(self._tmp_filename))
+
+        if self._conn is not None:
+            self._printer = self._conn.getDefault()
+            logging.info('Using printer "%s"', self._printer)
 
     def print(self, picture):
 
-        if isinstance(picture, ImageQt.ImageQt):
-            picture.save(self._tmp_filename)
-        else:
-            picture.save(self._tmp_filename, format='JPEG')
-
         if self._conn is not None:
+            if isinstance(picture, ImageQt.ImageQt):
+                picture.save(self._tmp_filename)
+            else:
+                picture.save(self._tmp_filename, format='JPEG')
             self._conn.printFile(self._printer, self._tmp_filename,
-                                 'photobooth %d' % self._counter, {})
+                                 "photobooth", {})
+
