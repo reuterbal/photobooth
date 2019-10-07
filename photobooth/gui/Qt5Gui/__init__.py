7f5e1459acb969d7e6e0aeee4098d4093fd8beca
Andreas Böhler <dev@aboehler.at>
2019-10-07 21:04:39 +0200

    Add stylesheets for 800x480px

--- photobooth/gui/Qt5Gui/__init__.py
+++ photobooth/gui/Qt5Gui/__init__.py
@@ -21,7 +21,9 @@
 styles = (('default', 'stylesheets/default.qss'),
           ('dark (1024 x 600 px)', 'stylesheets/dark-1024x600.qss'),
           ('dark (800 x 600 px)', 'stylesheets/dark-800x600.qss'),
+          ('dark (800 x 480 px)', 'stylesheets/dark-800x480.qss'),
           ('pastel (1024 x 600 px)', 'stylesheets/pastel-1024x600.qss'),
-          ('pastel (800 x 600 px)', 'stylesheets/pastel-800x600.qss'))
+          ('pastel (800 x 600 px)', 'stylesheets/pastel-800x600.qss'),
+          ('pastel (800 x 480 px)', 'stylesheets/pastel-800x480.qss'))
 
 from .PyQt5Gui import PyQt5Gui  # noqa
