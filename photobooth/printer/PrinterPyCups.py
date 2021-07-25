97d5589017048b27496b712300ebd8565554e564
Patrick Kanzler <dev@pkanzler.de>
2021-07-25 22:52:40 +0200

    fix access to num_prints

--- photobooth/printer/PrinterPyCups.py
+++ photobooth/printer/PrinterPyCups.py
@@ -27,7 +27,7 @@ import os
 try:
     import cups
 except ImportError:
-    logging.error('pycups is not installed')
+    logging.error("pycups is not installed")
     cups = None
 
 from PIL import ImageQt
@@ -36,7 +36,6 @@ from . import Printer
 
 
 class PrinterPyCups(Printer):
-
     def __init__(self, page_size, num_prints, print_pdf=False):
 
         self.num_prints = num_prints
@@ -44,13 +43,13 @@ class PrinterPyCups(Printer):
         self._conn = cups.Connection() if cups else None
 
         if print_pdf:
-            logging.error('Printing to PDF not supported with pycups')
+            logging.error("Printing to PDF not supported with pycups")
             self._conn = None
 
-        if os.access('/dev/shm', os.W_OK):
-            self._tmp_filename = '/dev/shm/print.jpg'
+        if os.access("/dev/shm", os.W_OK):
+            self._tmp_filename = "/dev/shm/print.jpg"
         else:
-            self._tmp_filename = '/tmp/print.jpg'
+            self._tmp_filename = "/tmp/print.jpg"
         logging.debug('Storing temp files to "{}"'.format(self._tmp_filename))
 
         if self._conn is not None:
@@ -59,12 +58,13 @@ class PrinterPyCups(Printer):
 
     def print(self, picture):
 
-        for n in range(num_prints):
-            logging.info('Printing picture, {} of {}'.format(n + 1, self.num_prints))
+        for n in range(self.num_prints):
+            logging.info("Printing picture, {} of {}".format(n + 1, self.num_prints))
             if self._conn is not None:
                 if isinstance(picture, ImageQt.ImageQt):
                     picture.save(self._tmp_filename)
                 else:
-                    picture.save(self._tmp_filename, format='JPEG')
-                self._conn.printFile(self._printer, self._tmp_filename,
-                                    "photobooth", {})
+                    picture.save(self._tmp_filename, format="JPEG")
+                self._conn.printFile(
+                    self._printer, self._tmp_filename, "photobooth", {}
+                )
